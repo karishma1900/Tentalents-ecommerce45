af50123f83101587eba39130e80a0e3d82934f87
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2025-08-06 17:23:12 +0530

    Update layout.tsx

--- apps/frontend/seller-ui/src/app/(routes)/dashboard/layout.tsx
+++ apps/frontend/seller-ui/src/app/(routes)/dashboard/layout.tsx
@@ -1,31 +1,25 @@
-'use client'
-import SideBarWrapper from '../../../shared/components/sidebar/sidebar'
+'use client';
 import React, { useState } from 'react';
+import SideBarWrapper from '../../../shared/components/sidebar/sidebar';
 import './page.css';
 
-const Layout = ({children}: {children:React.ReactNode}) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   return (
-    <div className='flex h-full min-h-screen'>
-      
+    <div className="layout-wrapper">
       {/* Sidebar */}
-      <div className='main-content'>
-        <div className="sticky top-0">
-          <SideBarWrapper
-            isMobileMenuOpen={isMobileMenuOpen}
-            onCloseMobileMenu={() => setIsMobileMenuOpen(false)}
-          />
-        </div>
-    
+      <SideBarWrapper
+        isMobileMenuOpen={isMobileMenuOpen}
+        onCloseMobileMenu={() => setIsMobileMenuOpen(false)}
+      />
 
-      {/* Main content */}
-      <main>
+      {/* Main Content */}
+      <main className="layout-content">
         {children}
       </main>
-      </div>
     </div>
   );
-}
+};
 
 export default Layout;
