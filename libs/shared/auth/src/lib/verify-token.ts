f41d20ab1a9ac6cfbc585839279fe2dcb4667dc7
Intecxo-tech <intecxo@gmail.com>
2025-08-15 22:48:02 +0530

    Update verify-token.ts

--- libs/shared/auth/src/lib/verify-token.ts
+++ libs/shared/auth/src/lib/verify-token.ts
@@ -1,7 +1,7 @@
 // pages/api/verify-token.ts
 
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { verifyToken } from '@shared/middlewares/auth/src/lib/jwt';
+import { verifyToken } from '@shared/auth/src/lib/jwt';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { accessToken } = req.body;
