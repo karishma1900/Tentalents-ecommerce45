a3bac843cf77f298a55153e7ceed455597999380
Intecxo-tech <intecxo@gmail.com>
2025-08-15 23:33:16 +0530

    Update kafka-client.ts

--- libs/shared/kafka/src/lib/kafka-client.ts
+++ libs/shared/kafka/src/lib/kafka-client.ts
@@ -1,6 +1,6 @@
 import { Kafka, KafkaConfig, Consumer } from 'kafkajs';
 import { kafkaConfig } from './kafka-config';
-import { logger } from '@shared/logger/';
+import { logger } from '@shared/logger';
 
 let kafka: Kafka | null = null;
 let consumer: Consumer | null = null;
