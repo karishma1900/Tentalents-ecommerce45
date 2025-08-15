a0d48c7253c65bd1883e0fc3c40a0fff47b247e3
Intecxo-tech <intecxo@gmail.com>
2025-08-15 23:34:26 +0530

    Update service.ts

--- libs/shared/email/src/lib/service.ts
+++ libs/shared/email/src/lib/service.ts
@@ -1,5 +1,5 @@
-import { sendEmail } from '@shared/email/';
-import { EmailPayload } from '@shared/email/';
+import { sendEmail } from '@shared/email';
+import { EmailPayload } from '@shared/email';
 
 export const sendEmailService = async (data: EmailPayload) => {
   await sendEmail(data);
