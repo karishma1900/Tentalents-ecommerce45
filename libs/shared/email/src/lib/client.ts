c4d39bc37b04e5f47959d0ab4903950fccab141d
Intecxo-tech <intecxo@gmail.com>
2025-08-15 22:47:35 +0530

    Update client.ts

--- libs/shared/email/src/lib/client.ts
+++ libs/shared/email/src/lib/client.ts
@@ -1,6 +1,6 @@
 import nodemailer from 'nodemailer';
 import { EmailPayload } from './types';
-import { env } from '@shared/middlewares/config/src/index';
+import { env } from '@shared/config/src/index';
 
 const transporter = nodemailer.createTransport({
   host: env.SMTP_HOST, // SendGrid SMTP: smtp.sendgrid.net
