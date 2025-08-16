d6df33f534c64176187b8331f34238e5f7195018
Intecxo-tech <intecxo@gmail.com>
2025-08-16 12:28:16 +0530

    Update kafka-config.ts

--- libs/shared/kafka/src/lib/kafka-config.ts
+++ libs/shared/kafka/src/lib/kafka-config.ts
@@ -1,9 +1,15 @@
 import { KafkaConfig, logLevel } from 'kafkajs';
 
-export const kafkaConfig: KafkaConfig & { groupId: string } = {
+export const kafkaConfig: KafkaConfig & { groupId: string, ssl: boolean, sasl: { mechanism: string, username: string, password: string } } = {
   clientId: process.env.KAFKA_CLIENT_ID || 'backend-client',
-  brokers: (process.env.KAFKA_BROKERS || 'kafka-broker.railway.internal:9092').split(','),
+  brokers: (process.env.KAFKA_BROKERS || 'd2g2jt1modb6qsnje0rg.any.ap-south-1.mpx.prd.cloud.redpanda.com:9092').split(','),
   logLevel: logLevel.INFO,
   groupId: process.env.KAFKA_GROUP_ID || 'backend-consumer-group',
-  // SSL/SASL options if needed...
+
+  ssl: true,
+  sasl: {
+    mechanism: 'scram-sha-256', // or 'scram-sha-512' based on your Redpanda credentials
+    username: process.env.KAFKA_USERNAME || 'Karishma', // replace with your Redpanda username or env var
+    password: process.env.KAFKA_PASSWORD || 'kWctuLCYqJcK0FQ037FehBoWVdOJa8', // replace with your Redpanda password or env var
+  },
 };
