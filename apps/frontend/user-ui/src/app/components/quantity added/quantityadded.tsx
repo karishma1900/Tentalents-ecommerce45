13fdd17c78c6a61a5638137b9820197065c4769f
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2025-08-16 20:28:47 +0530

    Update quantityadded.tsx

--- apps/frontend/user-ui/src/app/components/quantity added/quantityadded.tsx
+++ apps/frontend/user-ui/src/app/components/quantity added/quantityadded.tsx
@@ -36,7 +36,7 @@ type CartItem = {
 
 const QuantityAdded = ({ listCount = 2 }: ProductsProps) => {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
-  const CART_API_BASE_URL = process.env.NEXT_PUBLIC_CART_API_LINK ?? 'http://localhost:3020';
+  const CART_API_BASE_URL = process.env.NEXT_PUBLIC_CART_API_LINK ?? 'https://cart-service-z6ft.onrender.com/';
 
   useEffect(() => {
     fetchCartItems();
