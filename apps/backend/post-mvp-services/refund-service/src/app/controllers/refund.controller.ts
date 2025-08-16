0a86825c8587385cd0e865a2881369bc28f3542f
Intecxo-tech <intecxo@gmail.com>
2025-08-16 07:45:17 +0530

    Update refund.controller.ts

--- apps/backend/post-mvp-services/refund-service/src/app/controllers/refund.controller.ts
+++ apps/backend/post-mvp-services/refund-service/src/app/controllers/refund.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { refundService } from '../services/refund.service';
-import { sendSuccess } from '@shared/middlewares/utils/src/lib/response';
+import { sendSuccess } from '@shared/utils';
 
 export const requestRefund = async (
   req: Request,
