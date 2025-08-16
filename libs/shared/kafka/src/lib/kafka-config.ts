52d104cc57596246a939e5dc9f75c3635111c4c7
Intecxo-tech <intecxo@gmail.com>
2025-08-16 16:11:54 +0530

    Update kafka-config.ts

--- libs/shared/kafka/src/lib/kafka-config.ts
+++ libs/shared/kafka/src/lib/kafka-config.ts
@@ -1,8 +1,17 @@
+// import { KafkaConfig, logLevel } from 'kafkajs';
+
+// export const kafkaConfig: KafkaConfig & { groupId: string } = {
+//   clientId: process.env.KAFKA_CLIENT_ID || 'backend-client',
+//   brokers: (process.env.KAFKA_BROKERS || 'localhost:9092').split(','),
+//   logLevel: logLevel.INFO,
+//   groupId: process.env.KAFKA_GROUP_ID || 'backend-consumer-group',
+
+// };
 import { KafkaConfig, logLevel } from 'kafkajs';
 
 export const kafkaConfig: KafkaConfig & { groupId: string, ssl: boolean, sasl: { mechanism: string, username: string, password: string } } = {
   clientId: process.env.KAFKA_CLIENT_ID || 'backend-client',
-  brokers: (process.env.KAFKA_BROKERS || 'd2g2jt1modb6qsnje0rg.any.ap-south-1.mpx.prd.cloud.redpanda.com:9093').split(','),
+  brokers: (process.env.KAFKA_BROKERS || 'd2g2jt1modb6qsnje0rg.any.ap-south-1.mpx.prd.cloud.redpanda.com:9092').split(','),
   logLevel: logLevel.INFO,
   groupId: process.env.KAFKA_GROUP_ID || 'backend-consumer-group',
 
@@ -10,6 +19,6 @@ export const kafkaConfig: KafkaConfig & { groupId: string, ssl: boolean, sasl: {
   sasl: {
     mechanism: 'scram-sha-256', // or 'scram-sha-512' based on your Redpanda credentials
     username: process.env.KAFKA_USERNAME || 'Karishma', // replace with your Redpanda username or env var
-    password: process.env.KAFKA_PASSWORD || 'kWctuLCYqJcK0FQ037FehBoWVdOJa8', // replace with your Redpanda password or env var
+    password: process.env.KAFKA_PASSWORD || 'FmeFCWOtN139wzfesfss9ELtw4iWTg', // replace with your Redpanda password or env var
   },
 };
