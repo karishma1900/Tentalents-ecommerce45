f2d3161ec5e61185df64ffced1ba19da0d9b4ea8
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2025-08-16 20:27:42 +0530

    Update addaddress.tsx

--- apps/frontend/user-ui/src/app/components/addaddresspopup/addaddress.tsx
+++ apps/frontend/user-ui/src/app/components/addaddresspopup/addaddress.tsx
@@ -15,7 +15,7 @@ type AddAddressProps = {
   addressToEdit?: any;  // Added this prop for editing an existing address
 };
 
-const API_BASE_URL = 'http://localhost:3002'; 
+const API_BASE_URL = 'https://order-service-vck8.onrender.com/'; 
 
 const AddAddress = ({ isOpen, onClose, vendorId, addressToEdit, onAdd }: AddAddressProps) => {
   const formRef = useRef<HTMLDivElement>(null);
