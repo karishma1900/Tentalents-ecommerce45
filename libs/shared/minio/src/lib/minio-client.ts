0a168cf51628a172efa466509f3ff9af997a5c82
Intecxo-tech <intecxo@gmail.com>
2025-08-16 12:56:38 +0530

    Update minio-client.ts

--- libs/shared/minio/src/lib/minio-client.ts
+++ libs/shared/minio/src/lib/minio-client.ts
@@ -1,9 +1,9 @@
 import { Client } from 'minio';
 
 export const minioClient = new Client({
-  endPoint: process.env['MINIO_ENDPOINT']!,
-  port: parseInt(process.env['MINIO_PORT']!, 10),
-  useSSL: false,
+  endPoint: process.env['MINIO_ENDPOINT']!,         // No https://
+  port: parseInt(process.env['MINIO_PORT']!, 10),   // 443
+  useSSL: process.env['MINIO_USE_SSL'] === 'true',  // true
   accessKey: process.env['MINIO_ACCESS_KEY']!,
   secretKey: process.env['MINIO_SECRET_KEY']!,
 });
