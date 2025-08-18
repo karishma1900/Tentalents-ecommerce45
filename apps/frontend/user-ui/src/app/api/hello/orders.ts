d26a2270ba0b4d333e9031bcae97a70a6acc0bb1
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2025-08-18 10:04:58 +0530

    Update orders.ts

--- apps/frontend/user-ui/src/app/api/hello/orders.ts
+++ apps/frontend/user-ui/src/app/api/hello/orders.ts
@@ -13,7 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
 
       // Send the POST request to the external order service
-      const apiResponse = await fetch(`${orderApiUrl}/api/orders`, {
+      const apiResponse = await fetch(`https://order-service-ouw8.onrender.com/api/orders`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
