676dd147e4444301e37107e88c34927232a5290a
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2025-08-19 13:17:31 +0530

    Update page.tsx

--- apps/frontend/user-ui/src/app/(routes)/signup/page.tsx
+++ apps/frontend/user-ui/src/app/(routes)/signup/page.tsx
@@ -94,7 +94,7 @@ const SignUp = () => {
     try {
       const { email: enteredEmail } = getValues();
       await axios.post(
-        `${process.env.NEXT_PUBLIC_SERVER_URI}/api/auth/register/otp/initiate`,
+        `https://user-service-4i53.onrender.com/api/auth/register/otp/initiate`,
         { email: enteredEmail }
       );
       setEmail(enteredEmail);
@@ -125,7 +125,7 @@ const SignUp = () => {
     setLoading(true);
     try {
       await axios.post(
-        `${process.env.NEXT_PUBLIC_SERVER_URI}/api/auth/register/otp/verify`,
+        `https://user-service-4i53.onrender.com/api/auth/register/otp/verify`,
         {
           email,
           otp: otpCode,
@@ -143,7 +143,7 @@ const onSubmit = async (data: FormData) => {
   setLoading(true);
   try {
     await axios.post(
-      `${process.env.NEXT_PUBLIC_SERVER_URI}/api/auth/register/otp/complete`,
+      `https://user-service-4i53.onrender.com/api/auth/register/otp/complete`,
       {
         email,
         password: data.password,
@@ -187,7 +187,7 @@ const onSubmit = async (data: FormData) => {
 
     setLoading(true);
     try {
-      await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URI}/api/auth/register/otp/initiate`, {
+      await axios.post(`https://user-service-4i53.onrender.com/api/auth/register/otp/initiate`, {
         email,
       });
       setCanResend(false);
@@ -243,7 +243,7 @@ const handleGoogleCallback = async (response: any) => {
     console.log('Google ID Token:', response.credential);
 
     const res = await axios.post(
-      `${process.env.NEXT_PUBLIC_GOOGLE_LOGIN_API}`,
+      `https://user-service-4i53.onrender.com/api/auth/google-login}`,
       {
         provider: 'google',
         idToken: response.credential,
@@ -576,3 +576,4 @@ const handleFirebaseGoogleSignIn = async () => {
 };
 
 export default SignUp;
+
